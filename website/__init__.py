d390c5753515f95b73ca58af8eb85ab82498f5fb
whopperjames95 <james.goff1030@gmail.com>
2022-07-28 13:48:29 -0700

    finished project

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path #will need to delete after postgres
+from os import path
 from flask_login import LoginManager
 from flask_migrate import Migrate 
 import os
 
-# app = Flask(__name__)
+
 app =  Flask(__name__)
-app.config['SECRET_KEY'] = 'This is the secret key'
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #change URI to postgres, example found in Chalon's message
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://whopper95:J0o9i8u7@localhost:5432/aspirations'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 
 
 database = SQLAlchemy(app)
@@ -28,32 +26,3 @@ from website.authentication import auth
 app.register_blueprint(views, url_prefx='/') 
 app.register_blueprint(auth, url_prefx='/') 
 
-
-
-#inside terminal type in command = export FLASK_APP=(main.py) .....not sure here
-
-#also in terminal type = flask db init (this will create a migrations folder)
-#next in terminal type = flask db migrate -m "created bucket list table"
-#next in terminal type = flask db upgrade
-
-# def create_app():
-    
-
-
-    
-
-    
-
-    
-
-#     from .models import User, Note
-
-#     create_database(app)
-
-
-#     return app
-
-# def create_database(app):
-#     if not path.exists('website/' + DB_NAME):
-#         db.create_all(app=app)
-#         print('Created Database!')
